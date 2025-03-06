f5bee2a1643def102deeec87852fbdb5d90ebf65
Pau1R <rudnikov.paul@yandex.ru>
2025-03-07 00:59:35 +0500

    updated paths for config

--- Conf.py
+++ Conf.py
@@ -3,15 +3,17 @@ import os
 
 class Conf:
 
-	def __init__(self, app):
-		self.app = app
-		config = configparser.ConfigParser()
-		config.read("config.ini")
+    def __init__(self, app):
+        self.app = app
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        CONFIG_PATH = os.path.join(BASE_DIR, "config.ini")
+        config = configparser.ConfigParser()
+        config.read("config.ini")
 
-		self.api_key = config['onlinetestpad']['api_key']
-		self.students = config["onlinetestpad"]["students"].split(", ")
+        self.api_key = config['onlinetestpad']['api_key']
+        self.students = config["onlinetestpad"]["students"].split(", ")
 
-		self.token = config['github']['token']
-		self.gist_id = config['github']['gist_id']
-		self.gist_id_admin = config['github']['gist_id_admin']
-		self.filename = config['github']['filename']
+        self.token = config['github']['token']
+        self.gist_id = config['github']['gist_id']
+        self.gist_id_admin = config['github']['gist_id_admin']
+        self.filename = config['github']['filename']
